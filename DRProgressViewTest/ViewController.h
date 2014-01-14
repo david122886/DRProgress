15631e09d4c73e41822e470e7ca7bdf74a9bbab8
david.liu <liuchong1228@yahoo.cn>
2014-01-14 15:58:01 +0800

    Initial Commit

---
+++ DRProgressViewTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DRProgressViewTest
+//
+//  Created by david on 14-1-14.
+//  Copyright (c) 2014年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DRProgressViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DRProgressViewTest
+//
+//  Created by david on 14-1-14.
+//  Copyright (c) 2014年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
